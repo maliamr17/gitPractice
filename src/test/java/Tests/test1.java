b19e949048a90b96e21546b67bdb47372f99370b
amar mali <amardmali11@gmail.com>
2023-07-27 10:19:36 +0530

    old user commit

--- src/test/java/Tests/test1.java
+++ src/test/java/Tests/test1.java
@@ -5,7 +5,7 @@ public class test1 {
 	public static void main (String args[])
 	{
 		System.out.println("test case 1");
-		Sysout.out.println("test cases 2");
+		System.out.println("test cases 2");
 	}
 
 }
